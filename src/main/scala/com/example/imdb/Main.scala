f6f190d6a59edab3024716f992f2d4bb25a39388
deabreu1 <deabreu.ronaldo@gmail.com>
2021-02-14 14:18:28 +0000

    Call from main method.

--- src/main/scala/com/example/imdb/Main.scala
+++ src/main/scala/com/example/imdb/Main.scala
@@ -1,7 +1,11 @@
 package com.example.imdb
 
+import com.example.imdb.service.Service
+
 object Main extends App {
 
   // Start your application here
-  ???
+  val port: Option[Int] = None
+
+  new Service(port).run()
 }
